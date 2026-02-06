7d1776e33cec9b1028e980b78d53f331d9563574
Aljosa Makevic <aljosa100@gmail.com>
2026-02-06 16:35:15 +0100

    feat: add end-to-end script for DataHaven Storage with bucket creation, file upload, download, and integrity verification

---
+++ node/scripts-sc/end-to-end.ts
@@ -0,0 +1,85 @@
+import '@storagehub/api-augment';
+import { initWasm } from '@storagehub-sdk/core';
+import { polkadotApi } from './services/clientService.js';
+import {
+  downloadFile,
+  uploadFile,
+  verifyDownload,
+  waitForBackendFileReady,
+  waitForMSPConfirmOnChain,
+} from './operations/fileOperations.js';
+import { HealthStatus } from '@storagehub-sdk/msp-client';
+import { mspClient } from './services/mspService.js';
+import { createBucket, verifyBucketCreation, waitForBackendBucketReady } from './operations/bucketOperations.js';
+
+async function run() {
+  // Initialize WASM
+  await initWasm();
+
+  console.log('🚀 Starting DataHaven Storage End-to-End Script...');
+
+  // --8<-- [start:check-msp-health]
+  // 1. Check MSP Health
+  const mspHealth: HealthStatus = await mspClient.info.getHealth();
+  console.log('MSP Health Status:', mspHealth);
+  // --8<-- [end:check-msp-health]
+
+  // --8<-- [start:create-bucket]
+  // 2. Create Bucket
+  const bucketName = 'init-bucket-046';
+  const { bucketId, txReceipt } = await createBucket(bucketName);
+  console.log(`Created Bucket ID: ${bucketId}`);
+  console.log(`createBucket() txReceipt: ${txReceipt}`);
+  // --8<-- [end:create-bucket]
+  // const bucketId = '0xd25a71825b85583f68a1b7658c291bad4938203eb8910e1454e9cb3d8bb6c49b';
+
+  // --8<-- [start:verify-bucket]
+  // 3. Verify bucket exists on chain
+  const bucketData = await verifyBucketCreation(bucketId);
+  console.log('Bucket data:', bucketData);
+  // --8<-- [end:verify-bucket]
+
+  // --8<-- [start:wait-for-backend-bucket-ready]
+  // 4. Wait until indexer/backend knows about the bucket
+  await waitForBackendBucketReady(bucketId);
+  // --8<-- [end:wait-for-backend-bucket-ready]
+
+  // --8<-- [start:upload-file]
+  // 5. Upload file
+  const fileName = 'helloworld.txt';
+  const filePath = new URL(`./files/${fileName}`, import.meta.url).pathname;
+
+  const { fileKey, uploadReceipt } = await uploadFile(bucketId, filePath, fileName);
+  console.log(`File uploaded: ${fileKey}`);
+  console.log(`Status: ${uploadReceipt.status}\n`);
+  // --8<-- [end:upload-file]
+
+  // --8<-- [start:wait-for-msp-confirm-on-chain]
+  // 6. Wait for file to be ready in the network before downloading
+  await waitForMSPConfirmOnChain(fileKey.toHex());
+  await waitForBackendFileReady(bucketId, fileKey.toHex());
+  // await new Promise((resolve) => setTimeout(resolve, 5000));
+  console.log('passed?');
+
+  // --8<-- [end:wait-for-msp-confirm-on-chain]
+  // --8<-- [start:download-data]
+
+  // 7.. Download file
+  const downloadedFilePath = new URL('./files/helloworld_downloaded.txt', import.meta.url).pathname;
+  const downloadedFile = await downloadFile(fileKey, downloadedFilePath);
+  console.log(`File type: ${downloadedFile.mime}`);
+  console.log(`Downloaded ${downloadedFile.size} bytes to ${downloadedFile.path}`);
+  // --8<-- [end:download-data]
+
+  // --8<-- [start:verify-download]
+  // 8.. Verify download integrity
+  const isValid = await verifyDownload(filePath, downloadedFilePath);
+  console.log(`File integrity verified: ${isValid ? 'PASSED' : 'FAILED'}`);
+  // --8<-- [end:verify-download]
+
+  console.log('🚀 DataHaven Storage End-to-End Script Completed Successfully.');
+
+  await polkadotApi.disconnect();
+}
+
+run();
